d4d649a31a59b127d2d10f98aeef44df9a2ca4be
Divyanshu Bali <bali.divyanshu319@gmail.com>
2025-03-04 18:12:39 +0530

    Rework layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <>
+      <div className={styles.column1}>container 1</div>
+      <div className={styles.column2}>container 2</div>
+    </>
+  );
 }
