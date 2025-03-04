131fd5fc3d092ed4f05aa74c5196700b9bc6c2f2
Divyanshu Bali <bali.divyanshu319@gmail.com>
2025-03-04 19:05:27 +0530

    Landing image and logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
+import Image from "next/image";
 import styles from "./page.module.css";
+import EarthCenterMudBuilding from "../../public/assets/landing01.jpg";
 
 export default function Home() {
   return (
     <>
-      <div className={styles.column1}>container 1</div>
-      <div className={styles.column2}>container 2</div>
+      <div className={styles.column1}>
+        <div className={styles.landingImgContainer}>
+          <Image
+            alt="A picture of a mud building at Earth Center"
+            src={EarthCenterMudBuilding}
+            layout="fill"
+            objectFit="cover"
+            style={{ filter: "grayscale(1)" }}
+          />
+          <div className={styles.bgDark}></div>
+          <div className={styles.imaratLogoContainer}>
+            <div className={styles.logoTitle}>IMARAT ARCHITECTS</div>
+            <div className={styles.logoUnderline}></div>
+          </div>
+        </div>
+        <p className={styles.subtitle}>mud walls that curve, avoiding felling of trees, constructing amorphous spaces</p>
+      </div>
+      <div className={styles.column2}></div>
     </>
   );
 }
