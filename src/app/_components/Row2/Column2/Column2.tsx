515c9a20daa1a1d4cd1922d0e98930a397c6c491
Divyanshu Bali <bali.divyanshu319@gmail.com>
2025-03-17 15:50:33 +0530

    Add favicon, Column2 layout fix

--- src/app/_components/Row2/Column2/Column2.tsx
+++ src/app/_components/Row2/Column2/Column2.tsx
@@ -16,7 +16,7 @@ function Column2() {
       transition={{ delay: 0.2, duration: 1 }}
       className={styles.column2Container}
     >
-      <div className={styles.writing2}>
+      <div className={styles.writing1}>
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. In veniam
           unde est quibusdam ducimus at nihil quo, quisquam voluptatum illo
@@ -25,7 +25,7 @@ function Column2() {
         </p>
       </div>
 
-      <div className={styles.writing1}>
+      <div className={styles.writing2}>
         <h3>EMERGING ARCHITECT</h3>
       </div>
     </motion.div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,13 +8,17 @@ export const metadata: Metadata = {
   title: "Imarat Architects",
   description: "Imarat Architects",
   keywords: `architecture, building design, sustainable architecture, homes, construction, residence, design, interior design, exterior design`,
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
-export default function RootLayout(props: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout(
+  props: Readonly<{ children: React.ReactNode }>
+) {
   return (
     <AppProvider>
       <ClientRootLayout {...props} />
     </AppProvider>
   );
 }
-
