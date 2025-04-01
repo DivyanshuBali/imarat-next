ed0eb37fb4c54cac7abd37883af0abaa605b6439
Divyanshu Bali <bali.divyanshu319@gmail.com>
2025-04-01 19:48:49 +0530

    Add EBAA link to Bulletin

--- src/app/_components/Sidebar/Bulletin/Bulletin.tsx
+++ src/app/_components/Sidebar/Bulletin/Bulletin.tsx
@@ -1,63 +1,74 @@
 "use client";
 
-import React, { useState } from "react";
-import { AnimatePresence, motion } from "motion/react";
+import React from "react";
+import { motion } from "motion/react";
 import styles from "./Bulletin.module.css";
 import { useAppContext } from "@/app/_contexts/AppContext";
+import Link from "next/link";
 
 const BULLETINS_DATA = [
   {
     id: 0,
     date: "4 APRIL",
-    title: "WORKSHOP ON WOOD",
+    title: "EARTH BUILDERS ASSOCIATION OF AUSTRALIA",
+    link: "https://www.ebaa.asn.au/annual-conference/2025-ebaa-earth-building-conference-agm/",
   },
 ];
 
 function Bulletin() {
-  const [expandedCardIndex, setExpandedCardIndex] = useState<number | null>(
-    null
-  );
+  // const [expandedCardIndex, setExpandedCardIndex] = useState<number | null>(
+  //   null
+  // );
   const { interactionEnabled } = useAppContext();
 
-  const handleClick = (
-    e: React.KeyboardEvent<HTMLDivElement>,
-    id: number,
-    isExpanded: boolean
-  ) => {
-    if (e.key === "Enter" || e.key === " ") {
-      e.preventDefault();
-      setExpandedCardIndex(isExpanded ? null : id);
-    }
-  };
+  // const handleClick = (
+  //   e: React.KeyboardEvent<HTMLDivElement>,
+  //   id: number,
+  //   isExpanded: boolean
+  // ) => {
+  //   if (e.key === "Enter" || e.key === " ") {
+  //     e.preventDefault();
+  //     setExpandedCardIndex(isExpanded ? null : id);
+  //   }
+  // };
 
   return (
     <div className={styles.bulletinSection}>
       <div className={styles.divider} />
 
       {BULLETINS_DATA.map((b, index) => {
-        const isExpanded = expandedCardIndex === b.id;
+        // const isExpanded = expandedCardIndex === b.id;
 
         return (
-          <motion.div
-            className={styles.bulletin}
+          <Link
+            href={b.link}
             key={b.id}
-            onClick={() => setExpandedCardIndex(isExpanded ? null : b.id)}
-            onKeyDown={(e) => handleClick(e, b.id, isExpanded)}
-            role="button"
-            tabIndex={0}
-            aria-expanded={isExpanded}
-            aria-controls={`bulletin-content-${b.id}`}
-            initial={{ opacity: 0, x: 50 }}
-            animate={{
-              opacity: interactionEnabled ? 1 : 0,
-              x: interactionEnabled ? 0 : 50,
-            }}
-            transition={{ duration: 0.6, ease: "easeOut", delay: index * 0.2 }}
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <div className={styles.date}>{b.date}</div>
-            <div className={styles.title}>{b.title}</div>
+            <motion.div
+              className={styles.bulletin}
+              // onClick={() => setExpandedCardIndex(isExpanded ? null : b.id)}
+              // onKeyDown={(e) => handleClick(e, b.id, isExpanded)}
+              role="button"
+              tabIndex={0}
+              // aria-expanded={isExpanded}
+              // aria-controls={`bulletin-content-${b.id}`}
+              initial={{ opacity: 0, x: 50 }}
+              animate={{
+                opacity: interactionEnabled ? 1 : 0,
+                x: interactionEnabled ? 0 : 50,
+              }}
+              transition={{
+                duration: 0.6,
+                ease: "easeOut",
+                delay: index * 0.2,
+              }}
+            >
+              <div className={styles.date}>{b.date}</div>
+              <div className={styles.title}>{b.title}</div>
 
-            <AnimatePresence>
+              {/* <AnimatePresence>
               <motion.div
                 initial={{ height: 0, opacity: 0 }}
                 animate={{
@@ -68,8 +79,9 @@ function Bulletin() {
                 className={styles.bulletinContent}
                 transition={{ duration: 0.2 }}
               />
-            </AnimatePresence>
-          </motion.div>
+            </AnimatePresence> */}
+            </motion.div>
+          </Link>
         );
       })}
     </div>
