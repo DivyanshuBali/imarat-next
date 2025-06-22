a9b4c6b9a35a4ab99956efc9bce8241089b8e725
Divyanshu Bali <bali.divyanshu319@gmail.com>
2025-06-22 14:53:24 +0530

    Remove `CldImage` component
    This is a client component which was causing slow renders

--- src/app/_components/NextImageWrapper/NextImageWrapper.tsx
+++ src/app/_components/NextImageWrapper/NextImageWrapper.tsx
@@ -1,17 +1,7 @@
-"use client";
-
-import { CldImage } from "next-cloudinary";
 import Image, { ImageProps } from "next/image";
 import React from "react";
 
 function NextImageWrapper({ src, alt, ...rest }: ImageProps) {
-  if (
-    typeof src === "string" &&
-    src.startsWith("https://res.cloudinary.com/")
-  ) {
-    return <CldImage src={src} alt={alt} {...rest} />;
-  }
-
   return <Image src={src} alt={alt} {...rest} />;
 }
 
